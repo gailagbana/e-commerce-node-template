9a1605fd38c978a52dfdc39517a8cd59d96a9cd5
Kolawole Ojo <philliphopeojo@gmail.com>
2021-08-13 00:50:59 +0100

    Completed models configuration  file

--- src/models/_config.js
+++ src/models/_config.js
@@ -1,41 +1,42 @@
 /**
  * @author Oguntuberu Nathan O. <nateoguns.work@gmail.com>
-**/
+ * */
 require('dotenv').config();
-let glob = require('glob');
-let { resolve } = require('path');
+const glob = require('glob');
+const { resolve } = require('path');
 
-const {
-    APP_DB_URI
-} = process.env;
+const { APP_DB_URI } = process.env;
 
-let mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 module.exports.connect = () => {
     try {
-
-        mongoose.connect(APP_DB_URI, {
-            autoIndex: false,
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        }, (err, data) => {
-            if (err) {
-                console.log(`Could not connect to database`);
-                return;
+        mongoose.connect(
+            APP_DB_URI,
+            {
+                autoIndex: false,
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            },
+            (err) => {
+                if (err) {
+                    console.log('Could not connect to database');
+                    return;
+                }
+                console.log('Database connection established.');
             }
-
-            console.log(`Database connection established.`);
-        });
+        );
     } catch (e) {
         console.log(`DB Error: ${e.message}`);
     }
-}
+};
 
 module.exports.loadModels = () => {
-    let basePath = resolve(__dirname, '../models/');
-    let files = glob.sync('*.js', { cwd: basePath });
-    files.forEach(file => {
-        if ((file.toLocaleLowerCase()).includes('_config')) return;
+    const basePath = resolve(__dirname, '../models/');
+    const files = glob.sync('*.js', { cwd: basePath });
+    files.forEach((file) => {
+        if (file.toLocaleLowerCase().includes('_config')) return;
+        // eslint-disable-next-line
         require(resolve(basePath, file));
     });
-}
+};
